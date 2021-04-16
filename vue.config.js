2822632151522df446744247e784235d82cac513
ksugimori <12605906+ksugimori@users.noreply.github.com>
2021-04-16 23:49:28 +0900

    axios のセットアップ

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,15 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/',
-  outputDir: 'docs'
+  outputDir: 'docs',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'info'
+      }
+    }
+  }
 }
