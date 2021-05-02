cd9fec176701f090ae691017b5f21af4f1b5e2d8
ksugimori <ksugimori.private@gmail.com>
2021-05-02 18:12:40 +0900

    不要なコンポーネントを削除

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/pages/Home.vue'
 import Play from '@/components/pages/Play.vue'
-import Setting from '@/components/pages/Setting.vue'
 
 Vue.use(Router)
 
@@ -10,16 +8,8 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      component: Home
-    },
-    {
       path: '/play',
       component: Play
-    },
-    {
-      path: '/setting',
-      component: Setting
     }
   ]
 })
