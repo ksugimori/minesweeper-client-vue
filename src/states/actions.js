1610217535dd2de3d54da4466e129747a2607c1b
ksugimori <12605906+ksugimori@users.noreply.github.com>
2021-05-04 13:29:14 +0900

    細かな修正

--- src/states/actions.js
+++ src/states/actions.js
@@ -2,40 +2,45 @@ import axios from 'axios'
 import Game from '@/models/Game.js'
 
 const actions = {
+  /**
+   * フラグを立てる、外す
+   */
   async flag ({ commit, state }, point) {
-    const id = state.game.id
     const game = state.game
-    const cell = state.game.field.cellAt(point)
+    const cell = game.field.cellAt(point)
 
     if (cell.isFlag) {
-      await axios.delete(`/api/games/${id}/flags/x${point.x}y${point.y}`)
+      await axios.delete(`/api/games/${game.id}/flags/x${point.x}y${point.y}`)
 
       cell.unflag()
       commit('updateGame', { game })
     } else {
-      await axios.post(`/api/games/${id}/flags`, point)
+      await axios.post(`/api/games/${game.id}/flags`, point)
 
       cell.flag()
       commit('updateGame', { game })
     }
   },
 
+  /**
+   * セルを開く
+   */
   async open ({ commit, state }, point) {
-    const id = state.game.id
-
-    const response = await axios.post(`/api/games/${id}/open-cells`, point)
-
     const game = state.game
+
+    const response = await axios.post(`/api/games/${game.id}/open-cells`, point)
+
     game.open(response.data)
     commit('updateGame', { game })
 
     // ステータスが終了状態になっていないか確認
-    const status = await axios.get(`/api/games/${id}/status`).then(r => r.data.status)
+    const status = await axios.get(`/api/games/${game.id}/status`).then(r => r.data.status)
     if (status !== 'WIN' && status !== 'LOSE') {
       return
     }
 
-    const latestGame = await axios.get(`/api/games/${id}`).then(r => r.data)
+    // 終了していれば最新の状態を取得しなおす
+    const latestGame = await axios.get(`/api/games/${game.id}`).then(r => r.data)
 
     game.status = latestGame.status
     latestGame.mines.forEach(p => {
@@ -51,6 +56,9 @@ const actions = {
     commit('updateGame', { game })
   },
 
+  /**
+   * ゲームをリセットして新しく作成しなおす。
+   */
   async reset ({ commit }, setting) {
     const response = await axios.post('/api/games', { setting })
 
